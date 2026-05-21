1d2440c1dbafd58aef473737302b043afac8e722
Farid Rashidi <farid.rsh@gmail.com>
2026-05-20 21:55:35 -0400

    ✨ Add Visual Studio Code path

---
+++ home/dot_config/vscode/path.zsh
@@ -0,0 +1,3 @@
+if [[ -d "$HOME/Applications/Visual Studio Code.app/Contents/Resources/app/bin" ]]; then
+    export PATH="$HOME/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"
+fi
