050b28c6508fc71a11550cbb627c14756408229e
Farid Rashidi <farid.rsh@gmail.com>
2026-05-20 22:01:59 -0400

    🔧 Add npm configuration files

---
+++ home/dot_config/npm/env.zsh
@@ -0,0 +1,7 @@
+export NPM_CONFIG_PREFIX="$HOME/.local"
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+export NPM_CONFIG_CACHE="$XDG_CACHE_HOME/npm"
+
+export npm_config_prefix="$NPM_CONFIG_PREFIX"
+export npm_config_userconfig="$NPM_CONFIG_USERCONFIG"
+export npm_config_cache="$NPM_CONFIG_CACHE"
