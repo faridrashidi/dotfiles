15af70fadb25143e11a2bb33bf4e7c7a540bbebd
Farid Rashidi <farid.rsh@gmail.com>
2026-05-20 21:56:24 -0400

    ✨ Add Neovim configuration files

---
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+  },
+  -- Set it as your default colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
