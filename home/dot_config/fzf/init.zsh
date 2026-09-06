ceb8d544de610a0e04748b7fcc1bdb826e19a485
Farid Rashidi <farid.rsh@gmail.com>
2026-09-06 04:05:21 -0400

    🔧 Configure Atuin repository history

--- home/dot_config/fzf/init.zsh
+++ home/dot_config/fzf/init.zsh
@@ -1,1 +1,8 @@
-(($+commands[fzf])) && _cached_eval fzf --zsh
+if (($+commands[fzf])); then
+    if (($+widgets[atuin-search])); then
+        # Leave Ctrl-R to Atuin when its history widget is available.
+        FZF_CTRL_R_COMMAND="" _cached_eval fzf --zsh
+    else
+        _cached_eval fzf --zsh
+    fi
+fi
